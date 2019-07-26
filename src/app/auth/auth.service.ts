29e9c32d8b8d84d5d391d645beae03092c0e20e0
justJosh1004 <j.joson@live.com>
2019-07-26 13:51:39 -0400

    added createUser method in auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
-
 import { Store } from '@ngrx/store';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { environment } from './../../environments/environment';
 import { AuthData } from './auth-data.model';
 import * as fromRoot from '../app.reducer';
 import * as Auth from './auth.actions';
+
+const BACKEND_URL = `${environment.apiUrl}/users`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private store: Store<fromRoot.State>) {}
+  constructor(
+    private store: Store<fromRoot.State>,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  createUser(email: string, password: string) {
+    const authData: AuthData = { email, password };
+    return this.http.post(`${BACKEND_URL}/signup`, authData).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
 
   login(authData: AuthData) {
     this.store.dispatch(new Auth.SetAuthenticated());
